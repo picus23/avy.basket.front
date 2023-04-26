9ec58902038cc1cbdc62733642e6e2bbe3b35867
Misc1170 <sloopy1170@gmail.com>
2023-04-26 11:02:32 +0300

    error

--- src/Basket/BasketOpen.tsx
+++ src/Basket/BasketOpen.tsx
@@ -12,9 +12,9 @@ const BasketOpen = ({ }) => {
         <>
             <div className={'_ea587af69ebcf90d9e3b86ab139d841d'}>
 
-                {/* <div className="position-relative">
+                <div className="position-relative">
                     <Button icon={<MdShoppingCart size={24} fill={'gray'} />} btn_style={"gray"} counter={0} counter_style={"up"}><span className="fw-500">Корзина</span></Button>
-                </div> */}
+                </div>
 
 
                 <button type="button" className="btn position-relative btn-open my-btn-gray"
